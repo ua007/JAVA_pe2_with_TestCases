42ce549e91e6ad66fb4c10a17cb6b9348f3c6a1d
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-03 17:31:06 +0530

    Update EvenOddTest.java

--- src/main/test/com/stackroute/pe2/EvenOddTest.java
+++ src/main/test/com/stackroute/pe2/EvenOddTest.java
@@ -1,13 +1,27 @@
 package com.stackroute.pe2;
-
+import org.junit.After;
+import org.junit.Before;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class EvenOddTest {
+    
+    EvenOdd number;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        number=new EvenOdd();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        number=null;
+    }
     //test case for even no.
     @Test
     public void givenIntegerIsEvenReturnBoolean(){
-        EvenOdd number=new EvenOdd();
+       
         Boolean result;
         result=number.isEven(4);
         assertEquals(true,result);
@@ -15,7 +29,7 @@ public class EvenOddTest {
     //test case for not an even no.
     @Test
     public void givenIntegerIsNotEvenReturnBoolean(){
-        EvenOdd number=new EvenOdd();
+        
         Boolean result;
         result=number.isEven(5);
         assertEquals(false,result);
