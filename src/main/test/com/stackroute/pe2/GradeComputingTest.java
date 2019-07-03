76169b80264e43225fa36829075b2947c5d914c6
ua007 <35341797+ua007@users.noreply.github.com>
2019-07-03 17:33:51 +0530

    Update GradeComputingTest.java

--- src/main/test/com/stackroute/pe2/GradeComputingTest.java
+++ src/main/test/com/stackroute/pe2/GradeComputingTest.java
@@ -1,14 +1,28 @@
 package com.stackroute.pe2;
-
+import org.junit.After;
+import org.junit.Before;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GradeComputingTest {
+    
+    GradeComputing grd;
+
+    @Before
+    public void setUp() throws Exception
+    {
+        grd=new GradeComputing();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        grd=null;
+    }
     //test case for grade between 1 to 100
     @Test
     public void givenGradeIntegerArrayShouldReturnMessage()
     {
-        GradeComputing grd=new GradeComputing();
+       
         String result;
         result=grd.GradeComputingFunction(new int[]{56,67,45});
         assertEquals("The average is "+grd.checkAverage(new int[]{56,67,45})+"\n"+ "The minimum is "+grd.checkMin(new int[]{56,67,45})+ "\nThe maximum is "+grd.checkMax(new int[]{56,67,45}),result);
